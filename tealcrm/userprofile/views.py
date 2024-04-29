5995f62c0ee3bac0bfc1add86b366a84cb28890e
MrJayKay <107195209+MrJayKay@users.noreply.github.com>
2024-04-30 01:01:53 +0800

    admin added

---
+++ tealcrm/userprofile/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+def signup(request):
+    form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html', {
+        'form': form
+    })
+
+
