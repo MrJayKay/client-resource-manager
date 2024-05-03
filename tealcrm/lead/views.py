212fec4b7b38eb71a9f012bb08945bea1d005523
MrJayKay <107195209+MrJayKay@users.noreply.github.com>
2024-05-03 19:32:53 +0800

    added new lead form

--- tealcrm/lead/views.py
+++ tealcrm/lead/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .forms import AddLeadForm
+
 @login_required
 def add_lead(request):
-    return render(request, 'lead/add_lead.html')
+    form = AddLeadForm()
+    return render(request, 'lead/add_lead.html', {
+        'form': form
+    })
