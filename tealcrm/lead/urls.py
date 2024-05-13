3d9000386406be797ee343ec5e01b4a413113bc1
MrJayKay <107195209+MrJayKay@users.noreply.github.com>
2024-05-13 14:04:35 +0800

    fixed typo

--- tealcrm/lead/urls.py
+++ tealcrm/lead/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.leads_list, name='leads_list'),
-    path('<int:pk>/', views.leads_deatial,name='leads_detail'),
+    path('<int:pk>/', views.leads_detail,name='leads_detail'),
     path('add-lead/', views.add_lead, name='add_lead'),
 ]
