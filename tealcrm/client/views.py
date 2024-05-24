f04222830b1601f72b15622dfae6e24077ac875c
MrJayKay <107195209+MrJayKay@users.noreply.github.com>
2024-05-24 15:35:49 +0800

    Added client views

--- tealcrm/client/views.py
+++ tealcrm/client/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import AddClientForm
 from .models import Client
 
 @login_required
@@ -16,4 +18,25 @@ def clients_detail(request, pk):
 
     return render(request, 'client/clients_detail.html', {
         'client': client
+    })
+
+@login_required
+def clients_add(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.created_by = request.user
+            client.save()
+
+            messages.success(request, 'The client was created successfully.')
+
+            return redirect('clients_list')
+    
+    else:
+        form = AddClientForm()
+
+    return render(request, 'lead/clients_add.html', {
+        'form': form
     })
